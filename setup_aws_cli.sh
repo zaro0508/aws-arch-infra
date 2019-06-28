6698b75e3e36127f9e80ca3afae6f9e2625bfbd1
Khai Do <zaro0508@gmail.com>
2019-06-28 10:45:34 -0400

    deploy to both sandbox and burner from one repo

--- setup_aws_cli.sh
+++ setup_aws_cli.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -ex
 
+$ACCOUNT_NAME=$1
+
+# some projects deploy to multiple AWS accounts, dev/stage/prod/etc..
+# we manage this by using git branches, travis vars and double interpolation
+# of environment variables.
+eval export "AwsCfServiceRoleArn=\$AwsCfServiceRoleArn_$ACCOUNT_NAME"
+eval export "AwsTravisAccessKey=\$AwsTravisAccessKey_$ACCOUNT_NAME"
+eval export "AwsTravisSecretAccessKey=\$AwsTravisSecretAccessKey_$ACCOUNT_NAME"
+
+
 # setup the awscli with the correct aws account info.
 pip install awscli
 mkdir ~/.aws
