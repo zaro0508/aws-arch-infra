565f2ab50f73811aa2f5d33d743cef4e287c2407
Khai Do <zaro0508@gmail.com>
2019-06-28 17:31:06 -0700

    attempt to fix build

--- setup_aws_cli.sh
+++ setup_aws_cli.sh
@@ -13,6 +13,6 @@ eval export "AwsTravisSecretAccessKey=\$AwsTravisSecretAccessKey_$ACCOUNT_NAME"
 
 # setup the awscli with the correct aws account info.
 pip install awscli
-mkdir ~/.aws
+mkdir -p ~/.aws
 echo -e "[default]\nregion=us-east-1\nsource_profile=default\nrole_arn=$AwsCfServiceRoleArn" > ~/.aws/config
 echo -e "[default]\nregion=us-east-1\naws_access_key_id=$AwsTravisAccessKey\naws_secret_access_key=$AwsTravisSecretAccessKey" > ~/.aws/credentials
